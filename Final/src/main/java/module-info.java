6bdee294b19fc6ac872f7cdf41da15cecc1ea5ef
Mohammad <MohammadMokhtari3683@Gmail.com>
2023-07-10 21:23:17 +0330

    Reg & Log -> Hoora

--- Final/src/main/java/module-info.java
+++ Final/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.Final {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.Final to javafx.fxml;
