f7fd5b55094418d54ffc39d724fc6fe929712847
farzanms11 <farza2831@gmail.com>
2023-07-11 10:37:12 +0330

    CustomerRestaurantController CustomerRestaurant.fxml CustomerRestaurantTheme.css

--- Final/src/main/java/com/example/Final/FXMLController/FXCustomerRestaurantController.java
+++ Final/src/main/java/com/example/Final/FXMLController/FXCustomerRestaurantController.java
@@ -1,5 +1,59 @@
 package com.example.Final.FXMLController;
 
-public class FXCustomerRestaurantController {
+import com.example.Final.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+
+public class FXCustomerRestaurantController {
+    @FXML
+    VBox selectMenuVBox, totalMenuVbox, starterMenuVbox, mainMealMenuVbox, dessertMenuVbox, selectFoodVBox;
+    @FXML
+    Button backToCustomerMenu, backToMenuSelect;
+    public void openTotalMenu() {
+        selectFoodVBox.setVisible(false);
+        backToCustomerMenu.setVisible(false);
+        backToMenuSelect.setVisible(true);
+        totalMenuVbox.setVisible(true);
+    }
+    public void openStarterMenu() {
+        selectFoodVBox.setVisible(false);
+        backToCustomerMenu.setVisible(false);
+        backToMenuSelect.setVisible(true);
+        starterMenuVbox.setVisible(true);
+    }
+    public void openMainMealMenu() {
+        selectFoodVBox.setVisible(false);
+        backToCustomerMenu.setVisible(false);
+        backToMenuSelect.setVisible(true);
+        mainMealMenuVbox.setVisible(true);
+    }
+    public void openDessertMenu() {
+        selectFoodVBox.setVisible(false);
+        backToCustomerMenu.setVisible(false);
+        backToMenuSelect.setVisible(true);
+        dessertMenuVbox.setVisible(true);
+    }
+    public void goBackToMenuSelect() {
+        totalMenuVbox.setVisible(false);
+        starterMenuVbox.setVisible(false);
+        mainMealMenuVbox.setVisible(false);
+        dessertMenuVbox.setVisible(false);
+        selectMenuVBox.setVisible(true);
+    }
+    public void goBackToCustomerMenu() throws IOException {
+        FXMLLoader Loader = new FXMLLoader(Main.class.getResource("/fxml/CustomerMenu.fxml"));
+        Scene scene = new Scene(Loader.load());
+        Main.getStage().setScene(scene);
+    }
+    public void goBackToSelectFood() {
+        selectFoodVBox.setVisible(false);
+    }
+    public void foodSelected() {
+        selectFoodVBox.setVisible(true);
+    }
 }
